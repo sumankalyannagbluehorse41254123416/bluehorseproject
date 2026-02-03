ca224a93ce08fcbe249e841f8ccc479928808398
suman das <sumankrdas1999@gmail.com>
2026-02-03 16:20:53 +0530

    home

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/component/Header";
 import Footer from "@/component/Footer";
-
+import "swiper/css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
