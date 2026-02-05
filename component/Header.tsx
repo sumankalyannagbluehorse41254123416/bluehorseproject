4a9cf2095bc092d1924792a36d3d8993d6289a1b
suman das <sumankrdas1999@gmail.com>
2026-02-05 13:00:59 +0530

    changes

--- component/Header.tsx
+++ component/Header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Header = () => {
@@ -17,52 +18,45 @@ const Header = () => {
     <>
       <header
         className={`fixed top-0 left-0 w-full z-50 transition-all duration-300
-          ${
-            scrolled
-              ? "bg-white"
-              : "bg-transparent"
-          }
-        `}
-      >
+          ${scrolled ? "bg-white" : "bg-transparent"}
+        `}>
         <div className="mx-20">
           <nav className="flex items-center justify-between h-20">
             {/* LOGO */}
-            <a href="https://www.bluehorse.in">
+            <Link href="/">
               <img
                 src="https://www.bluehorse.in/assets/image/Common/Logo.png"
                 alt="BlueHorse Softwares logo"
                 className="h-auto w-[110px]"
               />
-            </a>
+            </Link>
 
             {/* NAV LINKS */}
             <div className="flex gap-8 text-sm font-semibold">
               {[
-                { name: "Work", link: "/work.html" },
-                { name: "About Us", link: "/about-us.html" },
-                { name: "Careers", link: "/careers.html" },
-                { name: "Contact", link: "/contact.html" },
-                { name: "Blog", link: "/blog.html" },
+                { name: "Work", link: "/work" },
+                { name: "About Us", link: "/about-us" },
+                { name: "Careers", link: "/careers" },
+                { name: "Contact", link: "/contact" },
+                { name: "Blog", link: "/blog" },
               ].map((item) => (
-                <a
+                <Link
                   key={item.name}
                   href={item.link}
                   className={`transition-colors duration-300
                     ${
-                      scrolled
-                        ? "text-blue-600 hover:text-blue-800"
-                        : "text-white hover:text-blue-300"
+                      scrolled ?
+                        "text-blue-600 hover:text-blue-800"
+                      : "text-white hover:text-blue-300"
                     }
-                  `}
-                >
+                  `}>
                   {item.name}
-                </a>
+                </Link>
               ))}
             </div>
           </nav>
         </div>
       </header>
-
     </>
   );
 };
