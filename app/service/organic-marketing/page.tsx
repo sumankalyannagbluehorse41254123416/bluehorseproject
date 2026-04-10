6fba17b24b313caa6e99c2e8ebe573dc5ceb41e6
Koushik Santra <koushik.bluehorse@gmail.com>
2026-04-10 12:17:29 +0530

    update

---
+++ app/service/organic-marketing/page.tsx
@@ -0,0 +1,14 @@
+
+import MarketBanner from '@/component/service/organic-marketing/MarketBanner'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <MarketBanner />
+      
+    </>
+  )
+}
+
+export default page
