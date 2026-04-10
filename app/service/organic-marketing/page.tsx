cdb2113730df3eba6d97e91dbf10f91f046c7446
Koushik Santra <koushik.bluehorse@gmail.com>
2026-04-10 15:03:30 +0530

    update

--- app/service/organic-marketing/page.tsx
+++ app/service/organic-marketing/page.tsx
@@ -1,11 +1,13 @@
 
 import MarketBanner from '@/component/service/organic-marketing/MarketBanner'
+import Servicesmarket from '@/component/service/organic-marketing/Servicesmarket'
 import React from 'react'
 
 const page = () => {
   return (
     <>
       <MarketBanner />
+      <Servicesmarket />
       
     </>
   )
